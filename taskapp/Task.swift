682e597acdc28c45d7a9c365cc83b8a275690e07
komiya346 <1mg9720s@komazawa-u.ac.jp>
2022-07-03 02:50:21 +0900

    7/3

---
+++ taskapp/Task.swift
@@ -0,0 +1,29 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by 越智三四郎 on 2022/06/27.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+
+    // タイトル
+    @objc dynamic var title = ""
+
+    // 内容
+    @objc dynamic var contents = ""
+
+    // 日時
+    @objc dynamic var date = Date()
+    // カテゴリ
+    @objc dynamic var category = ""
+
+    // id をプライマリーキーとして設定
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
